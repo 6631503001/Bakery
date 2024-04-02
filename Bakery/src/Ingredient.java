a369b0f6accf26f8774cb246e71bdf681d937dcf
koonnuthhh <157001558+6631503001@users.noreply.github.com>
2024-04-02 16:43:31 +0700

    set up root classes

--- Bakery/src/Ingredient.java
+++ Bakery/src/Ingredient.java
@@ -1,4 +1,35 @@
+import java.util.Random;
+
 public class Ingredient implements Bakery{
     private String Name;
-    
+    private int Number;
+
+    public int getNumber() {
+        return Number;
+    }
+    public void setNumber(int number) {
+        Number = number;
+    }
+
+    public Ingredient(String name,int number){
+        this.Name = name;
+        this.Number = number;
+    }
+    public Ingredient(String name){
+        this.Name = name;
+    }
+
+    @Override
+    public void Produce(){
+        Random random = new Random();
+        Number = random.nextInt(100);
+    }
+    @Override
+    public String Describe(){
+        return (this.Name + " is Ingredient");
+    }
+    @Override
+    public void DecreaseStock(int n){
+        Number = Number-n;
+    }
 }

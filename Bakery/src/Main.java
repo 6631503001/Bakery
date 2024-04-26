d3432eadd801b7ca117f623f819e1f895edfe95c
koonnuthhh <157001558+6631503001@users.noreply.github.com>
2024-04-27 05:18:45 +0700

    Patch 1.0
    -Finished sellBakery
    -make a little change to main menu
    -Day now can be functional after End program normally it assume that one day have pass

--- Bakery/src/Main.java
+++ Bakery/src/Main.java
@@ -28,7 +28,7 @@ public class Main {
                     System.out.println("We have " + money + " Baht");
                     System.out.println("What do you want to do?");
                     System.out.println(
-                            "1.) Edit bakery ingredient\n2.) Show&BuyIngredients in stock\n3.) Produce bakery\n4.) Sell Bakery\n5.) Manage Money\n6.) End program");
+                            "1.) Edit bakery ingredient\n2.) Show&BuyIngredients in stock\n3.) Produce bakery\n4.) Sell Bakery\n5.) Manage Money\n<input 0 to go to End program>");
                     if (scan.hasNextInt()) {
                         int choice = Integer.parseInt(scan.nextLine());
                         // clearScreen();
@@ -46,26 +46,25 @@ public class Main {
                                 ProduceBakery();
                                 break;
                             case 4:
-                            clearScreen();
-                            SellBakery();
+                                clearScreen();
+                                SellBakery();
                                 break;
                             case 5:
                                 clearScreen();
                                 ManageMoney();
                                 break;
-                            case 6:
+                            case 0:
+                                Day += 1;
                                 Bakesomething = false;
                                 break;
                             default:
                                 clearScreen();
                                 Banner();
-                                System.out.println("Please only select options 1 - 6");
-
+                                System.out.println("Please only select options 0 - 5");
                                 break;
                         }
                         writeBakeryStore(Bakes, Allingredients);
                     } else {
-
                         clearScreen();
                         Banner();
                         System.out.println("Invalid input. Please enter a number");
@@ -128,7 +127,6 @@ public class Main {
         Bake selected;
         String input;
         boolean program = true, subprogram = true;
-
         while (program) {
             try {
                 Number = 1;
@@ -136,11 +134,8 @@ public class Main {
                 for (Bake a : Bakes) {
                     System.out.println(Number + ". " + a.getName());
                     Number++;
-
                 }
-
                 System.out.println("Select number to see their ingredients or insert 0 to main menu");
-
                 choices = Integer.parseInt(scan.nextLine());
                 if (choices == 0) {
                     break;
@@ -149,17 +144,16 @@ public class Main {
                 clearScreen();
                 subprogram = true;
                 while (subprogram) {
-                    ;
                     // to show all the ingredient to use
                     boolean a = true;
                     while (a) {
-                    Number = 0;
-                    System.out.println(selected.getName() + " Use");
-                    for (Ingredient n : selected.Ingredients) {
-                        System.out.print(n.getName() + " " + selected.UseIngredient.get(Number) + " ");
-                        Number++;
-                    }
-                    System.out.println("\nDo you want to edit Ingredient?");
+                        Number = 0;
+                        System.out.println(selected.getName() + " Use");
+                        for (Ingredient n : selected.Ingredients) {
+                            System.out.print(n.getName() + " " + selected.UseIngredient.get(Number) + " ");
+                            Number++;
+                        }
+                        System.out.println("\nDo you want to edit Ingredient?");
                         System.out.println("1 to increase ingredient , 2 to decrease ingredient, 3 to not edit");
                         try {
                             choices = Integer.parseInt(scan.nextLine());
@@ -200,16 +194,13 @@ public class Main {
                                     scan.nextLine();
                                     clearScreen();
                                 }
-                            }
-
-                            else {
+                            } else {
                                 System.out.println("Your Ingredient doesn't exist in our dictory");
                                 scan.nextLine();
                                 System.out.print("\033[H\033[2J");
                             }
                             clearScreen();
                             break;
-
                         case 2:
                             System.out.println("Please Insert your ingredient to decreaes");
                             input = scan.nextLine();
@@ -220,30 +211,25 @@ public class Main {
                                     Number = Integer.parseInt(scan.nextLine());
                                     clearScreen();
                                     selected.decreaesIngredient(in2, Number);
-
                                 } catch (NumberFormatException e) {
                                     System.out.println("Input only number pls.");
                                 }
-
                             } else {
                                 System.out.println("Your Ingredient doesn't exist in our dictory(?)");
                                 scan.nextLine();
                                 clearScreen();
                             }
                             break;
-
                         case 3:
                             subprogram = false;
                             clearScreen();
                             break;
-
                         default:
                             clearScreen();
                             System.out.println("Please select only avaliable number");
                             break;
                     }
                 }
-
             } catch (Exception e) {
                 clearScreen();
                 System.out.println("Please select only avaliable number");
@@ -276,23 +262,24 @@ public class Main {
                     selected = Allingredients.get(choices - 1);
                     while (true) {
                         try {
-                            
+
                             System.out.println(selected.getName() + " is " + selected.getPrice() + " Baht");
                             System.out.println("We have " + selected.getNumber() + " of " + selected.getName());
                             System.out.println("How many do you want to buy?\nOr enter 0 to go back");
                             Number = -1;
                             Number = Integer.parseInt(scan.nextLine());
-                            int cost= Number*selected.getPrice();
+                            int cost = Number * selected.getPrice();
                             if (Number > 0) {
-                                if(money >= cost){
-                                selected.setNumber(selected.getNumber() + Number);
-                                money -= cost;
-                                System.out.println("Buy succesfully\nYou have " + money + "Baht");
-                                scan.nextLine();
-                                clearScreen();
-                                break;
-                                }else{
-                                    System.out.println("Buy unsuccesfully\nYou have " + money + "Baht But it cost " + cost + " Baht");
+                                if (money >= cost) {
+                                    selected.setNumber(selected.getNumber() + Number);
+                                    money -= cost;
+                                    System.out.println("Buy succesfully\nYou have " + money + "Baht");
+                                    scan.nextLine();
+                                    clearScreen();
+                                    break;
+                                } else {
+                                    System.out.println("Buy unsuccesfully\nYou have " + money + "Baht But it cost "
+                                            + cost + " Baht");
                                     scan.nextLine();
                                 }
                             } else if (Number == 0) {
@@ -301,7 +288,7 @@ public class Main {
                             } else {
                                 System.out.println("Cannot insert less than 0!!");
                             }
-                            //clearScreen();
+                            // clearScreen();
                         } catch (Exception e) {
                             clearScreen();
                             System.out.println("Please Enter only integer");
@@ -351,7 +338,7 @@ public class Main {
                     }
 
                     System.out.println("Which one do you want to produce?");
-                    System.out.println("<input 0 to go back to Produce menu>");
+                    System.out.println("<input 0 to go to Produce menu>");
                     try {
                         choice = Integer.parseInt(scan.nextLine());
                         if (choice == 0) {
@@ -411,75 +398,85 @@ public class Main {
 
         }
     }
-        //check stock
-        private static boolean Checkstock(Bake a,int num){
-            if(a.getNumber()-num <0){
-                return true;
+
+    // check stock
+    private static boolean Checkstock(Bake a, int num) {
+        if (a.getNumber() - num < 0) {
+            return true;
+        }
+        return false;
+    }
+
+    // subprogram 4
+    private static void SellBakery() {
+        int i = 1;
+        int input = 0;
+        Bake Customerchoose = new Bake(null);
+        //int price = 0;
+        while (true) {
+            try {
+                input = -1;
+                //price = 0;
+                i = 1;
+                System.out.println("Welcome to our Bakery store here is what we have");
+
+                for (Bake a : Bakes) {
+                    System.out.println(i + ". " + a.getName() + " have " + a.getNumber() + " piece(s) ");
+                    i++;
+                }
+                System.out.println("please select your menu <input 0 to back to main menu>");
+
+                input = Integer.parseInt(scan.nextLine());
+                // System.out.println(input);
+                if (input == 0) {
+                    clearScreen();
+                    break;
+                }
+                Customerchoose = Bakes.get(input - 1);
+                while (true) {
+                    input = 0;
+                    System.out.println("How many " + Customerchoose.getName() + " do you want?\n<input 0 to back to menu>");
+                    try {
+                        input = Integer.parseInt(scan.nextLine());
+                        if (input == 0){
+                            clearScreen();
+                            break;
+                        }else if (Checkstock(Customerchoose, input)) {
+                            System.out.println("We dont have enough.");
+                        } else{
+                            Customerchoose.setNumber(Customerchoose.getNumber()-input);
+                            System.out.println("You got "+input+" pices of "+Customerchoose.getName()+".");
+                            System.out.println("Price is "+Customerchoose.getPrice()*input+" Bath.");
+                            money += Customerchoose.getPrice()*input;
+                            scan.nextLine();
+                            clearScreen();
+                            break;
+                        }
+                    } catch (Exception e) {
+                        System.out.println("Number only!!");
+                    }
+                }
+            } catch (NumberFormatException e) {
+                clearScreen();
+                System.out.println("Number only!");
+            } catch (IndexOutOfBoundsException e) {
+                clearScreen();
+                System.out.println("We dont have that menu.");
             }
-            return false;
+
         }
-       
-            
-     // subprogram 3
-    private static void SellBakery(){
-    int i = 1;
-    int input=0;
-    Bake Customerchose = new Bake(null);
-    int price =0;
-    while (true) {
-        
-        price =0;
-        i = 1;
-        System.out.println("Welcome to our Bakery store here is what we have");
-         
-        for(Bake a: Bakes)
-         {
-            System.out.println(i+". "+a.getName()+" have "+a.getNumber()+" pices ");
-            i++;        
-        }
-       do{
-
-        while(true){
-         System.out.println("pls select your menu <input 0 to back to menu>");
-         try{
-            input = Integer.parseInt(scan.nextLine());
-            try{
-            Customerchose = Bakes.get(input -1);
-            break;
-         }catch(IndexOutOfBoundsException e ){
-            System.out.println("We dont have that menu.");
-         }
-         }catch(NumberFormatException e){
-            System.out.println("Number only!");
-         }
-         
-        }
-         
-
-         System.out.println("How many "+Customerchose.getName()+" do you want ?");
-         try{
-            input = Integer.parseInt(scan.nextLine());
-         }catch(NumberFormatException e){
-            System.out.println("Number only!");
-         }
-         if(Checkstock(Customerchose, input)){
-            System.out.println("We dont have enough.");
-         }
-
-        } while(Checkstock(Customerchose, input));
-
-        Customerchose.setNumber(Customerchose.getNumber()-input);
-        System.out.println("You got "+input+" pices of "+Customerchose.getName()+" .");
-        System.out.println("Price is "+Customerchose.getPrice()*input+" Bath.");
-        money =money+price;
-        clearScreen();
+        /*
+         * 
+         * while(Checkstock(Customerchose, input));
+         * 
+         * Customerchose.setNumber(Customerchose.getNumber()-input);
+         * System.out.println("You got "+input+" pices of "+Customerchose.getName()+" ."
+         * );
+         * System.out.println("Price is "+Customerchose.getPrice()*input+" Bath.");
+         * money =money+price;
+         * clearScreen();
+         */
     }
-    }
-
-
-
-
-
 
     // Subprogram 5
     private static void ManageMoney() {
@@ -504,21 +501,21 @@ public class Main {
                                 clearScreen();
                                 break;
                             } else if (deposit < 0) {
-                                if((money += deposit) < 0){
+                                if ((money += deposit) < 0) {
                                     System.out.println("You can't Withdraw more than you have!!");
                                     scan.nextLine();
                                     clearScreen();
                                     break;
-                                }else{
-                                money += deposit;
-                                System.out.println("Withdraw success!!");
-                                scan.nextLine();
-                                clearScreen();
-                                break;
+                                } else {
+                                    money += deposit;
+                                    System.out.println("Withdraw success!!");
+                                    scan.nextLine();
+                                    clearScreen();
+                                    break;
                                 }
                             }
-                            
-                        } catch (Exception e) { 
+
+                        } catch (Exception e) {
                             System.out.println("Please Enter only number");
                         }
                     }
@@ -602,7 +599,7 @@ public class Main {
     public static void writeBakeryStore(List<Bake> Bakes, List<Ingredient> Allingredients) {
         try {
             BufferedWriter Store = new BufferedWriter(new FileWriter(FileBakery));
-            Store.write(Filename1 + " " + Filename2 + " " + money);
+            Store.write(Filename1 + " " + Filename2 + " " + money + " " + Day);
             Store.close();
         } catch (Exception e) {
             System.out.println("SomethingWrongHappen");
@@ -621,7 +618,7 @@ public class Main {
 
             BufferedWriter addmaterial = new BufferedWriter(new FileWriter(Filename2));
             for (Ingredient b : Allingredients) {
-                addmaterial.write(b.getName() + " " + b.getNumber() + " " + b.getPrice() +"\n");
+                addmaterial.write(b.getName() + " " + b.getNumber() + " " + b.getPrice() + "\n");
             }
             addmaterial.close();
 
@@ -630,12 +627,9 @@ public class Main {
         }
     }
 
-    // This method will called at the start of day it will make set variable
-    // arraylist
-    // Then it will clear file to prepare the write method called at the end of the
-    // day
     public static void readBakeryStore() {
         String line = null;
+        // ForReadBakeryStoreFile
         int i;
         try {
             BufferedReader readStore = new BufferedReader(new FileReader(FileBakery));
@@ -645,6 +639,7 @@ public class Main {
                 Filename1 = fromStore[0];
                 Filename2 = fromStore[1];
                 money = Integer.parseInt(fromStore[2]);
+                Day = Integer.parseInt(fromStore[3]);
                 i++;
             }
             readStore.close();
@@ -659,23 +654,12 @@ public class Main {
             i = 0;
             while ((line = readingredient.readLine()) != null) {
                 String fromfile2[] = line.split("\\s+");
-                Ingredient addsome = new Ingredient(fromfile2[0], Integer.parseInt(fromfile2[1]), Integer.parseInt(fromfile2[2]));
+                Ingredient addsome = new Ingredient(fromfile2[0], Integer.parseInt(fromfile2[1]),
+                        Integer.parseInt(fromfile2[2]));
                 Allingredients.add(addsome);
-
-                /*for (String a : fromfile2) {
-                    Ingredient addsome = new Ingredient(a);
-                    try {
-                        int number = Integer.parseInt(a);
-                        Allingredients.get(i).setNumber(number);
-                    } catch (NumberFormatException e) {
-                        Allingredients.add(addsome);
-                    }
-                }*/
                 i++;
-
             }
             readingredient.close();
-
             BufferedReader readbake = new BufferedReader(new FileReader(Filename1));
             while ((line = readbake.readLine()) != null) {
                 String[] fromfile = line.split("\\s+");
@@ -684,7 +668,6 @@ public class Main {
                     int price = Integer.parseInt(fromfile[1]);
                     Bake rebake = new Bake(name, price);
                     Bakes.add(rebake);
-
                     for (int o = 2; o < fromfile.length; o += 2) {
                         String ingredientName = fromfile[o];
                         int useingredient = Integer.parseInt(fromfile[o + 1]);
@@ -703,7 +686,4 @@ public class Main {
             System.out.println("Something happen with your file.");
         }
     }
-
 }
-// if you want to creat method clearfile separate from
-// ReadbakeryrecipesAndIngredient just tell me
